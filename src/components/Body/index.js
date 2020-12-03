d3b10d3d547ae66b98b8f68930ed1b1ade08c690
Mike <mike@mikevitelli.com>
2020-12-03 17:28:01 -0500

    refactored

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Body = ({users}) => {
-    return ( 
-        // mapped user array in rows
+const Body = ({ users }) => {
+  return (
+    // mapped user array in rows
 
-        
-     );
-}
- 
+    <div></div>
+  );
+};
+
 export default Body;
